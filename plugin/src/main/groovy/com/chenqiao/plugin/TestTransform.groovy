68dda2a5889cb453f95cbda638c3fd3b4566ac35
Joe Chen <mrjctech@gmail.com>
2020-04-07 21:41:28 +0800

    Update TestTransform.groovy

--- plugin/src/main/groovy/com/chenqiao/plugin/TestTransform.groovy
+++ plugin/src/main/groovy/com/chenqiao/plugin/TestTransform.groovy
@@ -30,6 +30,15 @@ class TestTransform extends Transform{
         return TransformManager.CONTENT_CLASS
     }
 
+        //    指Transform要操作内容的范围，官方文档Scope有7种类型：
+//    EXTERNAL_LIBRARIES        只有外部库
+//    PROJECT                       只有项目内容
+//    PROJECT_LOCAL_DEPS            只有项目的本地依赖(本地jar)
+//    PROVIDED_ONLY                 只提供本地或远程依赖项
+//    SUB_PROJECTS              只有子项目。
+//    SUB_PROJECTS_LOCAL_DEPS   只有子项目的本地依赖项(本地jar)。
+//    TESTED_CODE                   由当前变量(包括依赖项)测试的代码
+//    SCOPE_FULL_PROJECT        整个项目
     @Override
     Set<? super QualifiedContent.Scope> getScopes() {
         return TransformManager.SCOPE_FULL_PROJECT
